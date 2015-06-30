dddc4735849881488b16fbc0d076b83b59515e3d
tbeauvais <tbeauvais1@gmail.com>
2015-06-30 19:37:13 -0400

    Add sample message and notification

---
+++ lib/notifier.rb
@@ -0,0 +1,19 @@
+require 'grocer'
+
+class Notifier
+
+  def self.pusher
+    $stderr.puts "!!!!! " + cert_file
+    @pusher ||= Grocer.pusher(certificate: cert_file, gateway:  'gateway.sandbox.push.apple.com',  port: 2195,  retries:  1, passphrase: 'Channelx0628')
+  end
+
+  def self.send_notification(token, message)
+    notification = Grocer::Notification.new(device_token: token,  alert: message,  badge: 1, sound: 'default')
+    pusher.push(notification)
+  end
+
+  def self.cert_file
+    File.join(File.dirname(__FILE__), '..', 'certs', 'apple_push_notification.pem')
+  end
+
+end
--- routes/emails.rb
+++ routes/emails.rb
@@ -1,3 +1,5 @@
+require 'notifier'
+
 class App < Sinatra::Base
 
   namespace '/api/v1' do
@@ -5,10 +7,31 @@ class App < Sinatra::Base
     post '/emails' do
       content_type :json
       status 201
+      email = parse_email(request)
+      subject = email['headers']['Subject']
+
+      # temporary message
+      message = {
+        name: 'SUNNY\'S',
+        lastText: subject,
+        logo: 'https://static.ctctcdn.com/galileo/images/templates/Galileo-Template-Images/FeedbackRequest/FeedbackRequest_VerticalLogo.png',
+        link: 'https://s3.amazonaws.com/channelx/florist.html'
+      }
+
+      message_access.add(message)
+
+      Notifier.send_notification('6533903318951d6fe40616f238ad325219f824638535272ea4b8b626d86eae4b', subject)
+
       File.open('email.json', 'w') {|f| f.write(request.body.read) }
       json success: 'Created'
     end
 
   end
+
+  def parse_email(request)
+    JSON.parse(request.body.read)
+  end
+
+
 
 end
