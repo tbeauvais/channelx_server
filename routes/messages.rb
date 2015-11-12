865234575eca56dd53d32495c410603f32863727
Ryan L'Italien <rlitalien@constantcontact.com>
2015-11-12 13:54:19 -0500

    Add messages subscribe endpoint

--- routes/messages.rb
+++ routes/messages.rb
@@ -18,6 +18,36 @@ class App < Sinatra::Base
       {id: message['id']}.to_json
     end
 
+    # Param
+    # :id - id of business
+    # :notify - if true, send notification to user
+    # :type - 'Flowers' or 'Petstore'
+    post '/messages/:id/subscribe' do
+      content_type :json
+      status 201
+      message = JSON.parse(request.body.read)
+
+      if message['type']
+        if message['type'] == 'Flowers'
+          flower = 'https://innojam-channel-x.s3.amazonaws.com/221e6bd0-6b99-0133-47d5-46a2714e2106.html'
+          message.store('link', flower)
+        elsif message['type'] == 'Petstore'
+          petstore = 'https://innojam-channel-x.s3.amazonaws.com/0e1fdaa0-6b9a-0133-47d5-46a2714e2106.html'
+          message.store('link', petstore)
+        end
+      end
+
+      message_access.add(message)
+
+      if params[:notify]
+        device_access.fetch_all.each do |device|
+          Notifier.send_notification(device['token'], message[:lastText])
+        end
+      end
+
+      {id: message['id']}.to_json
+    end
+
     delete '/messages/:id' do
       content_type :json
       status 204
